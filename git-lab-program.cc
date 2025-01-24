020f772593f71c59433ac8e5e3e45fba2df1425f
Alex Kavalec <ak544822@ohio.edu>
2025-01-24 16:16:23 -0500

    1/24/25

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Alex Kavalec
+ *        Date: 1/24/25
+ * Description: Learning how to use github with linux in VS Code
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
